02b407045110e352763d705c0d23833d556ca28d
Alekseyya <alekseyyarchuk@gmail.com>
2017-04-04 20:41:19 +0300

    Добавлен класс Book

---
+++ Home_Work_3/Task_1/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    //PoCo класс
+    class Book
+    {
+        private string Title; // Название.
+        private string Author; // Автор.
+        private decimal Price; // Цена.
+        private bool CSharp; // Книга о C#
+
+        public delegate void ProcessBookDelegate(Book book);
+
+        public Book(string Title, string Autor, decimal Price, bool CSharp) //Писать в конструкторе тру/фолс на кажлой книге фигня(передалать)
+        {
+            this.Title = Title;
+            this.Author = Author;
+            this.Price = Price;
+            this.CSharp = CSharp;
+        }
+    }
+}
---
+++ Home_Work_3/Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Ярчук Алексей
+/// <summary>
+/// Разработать класс описывающий библиотеку, Bookstore. Класс должен поддерживать возможность добавлять книги в библиотеку, метод AddBook, и обрабатывать книги, метод ProcessBooks(ProcessBookDelegate processBook).
+/// Книги в библиотеке имеют следующие атрибуты:
+/// public string Title; // Название.
+/// public string Author; // Автор.
+/// public decimal Price; // Цена.
+/// public bool CSharp; // Книга о C#
+/// Используя делегат public delegate void ProcessBookDelegate(Book book)
+/// 1) распечатать названия всех книг о С# отсортировав их по убыванию цены в консоли
+/// 2) вычислить и отобразить общую стоимость всех книг о С# в консоли
+/// 3) вычислить и отобразить среднюю стоимость книг о С# в консоли.
+/// Расчет общей и средней стоимости реализовать в отдельном классе.
+/// Интерфейс для реализации сортировки IComparable.
+/// </summary>
+
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
