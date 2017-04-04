c8bcf83edf09feb248337ae2a1eaf2a676920a53
Alekseyya <alekseyyarchuk@gmail.com>
2017-04-04 21:03:49 +0300

    Добавлен класс библиотеки и интерфейсы с ним связаные

--- Home_Work_3/Task_1/Book.cs
+++ Home_Work_3/Task_1/Book.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 namespace Task_1
 {
     //PoCo класс
-    class Book
+    public class Book
     {
         private string Title; // Название.
         private string Author; // Автор.
         private decimal Price; // Цена.
         private bool CSharp; // Книга о C#
 
-        public delegate void ProcessBookDelegate(Book book);
+        
 
         public Book(string Title, string Autor, decimal Price, bool CSharp) //Писать в конструкторе тру/фолс на кажлой книге фигня(передалать)
         {
---
+++ Home_Work_3/Task_1/Library.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class Library : IEnumerable<Book>, ILibrary
+    {
+        private List<Book> library;
+        public delegate void ProcessBookDelegate(Book book);
+
+        public Library()
+        {
+            this.library = new List<Book>();
+
+        }
+
+        public void AddBook()
+        {
+            
+        }
+       
+
+        public IEnumerator<Book> GetEnumerator() => library.GetEnumerator();
+
+        public void ProcessBook(ProcessBookDelegate processBookDelegate)
+        {
+            
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        
+    }
+}
