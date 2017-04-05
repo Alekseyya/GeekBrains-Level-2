41c5edda2fb846bb31f5f1c1dd4eb1550823aebb
Alekseyya <alekseyyarchuk@gmail.com>
2017-04-05 16:56:49 +0300

    Добавление в событие дженерик делегата

---
+++ Home_Work_3/Task_2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Ярчук Алексей
+
+///Добавьте в пример Lesson3 обобщенный делегат
+///
+namespace Task_2
+{
+    public delegate void MethodContainer<T> (T value);
+
+    
+    class ClassCounter<T>
+    {
+        public event MethodContainer<T> onCount;
+        public void Count()
+        {
+            
+            for (int i = 0; i < 20; i++)
+            {
+                if(i==20)
+                {
+                    onCount(T);
+                }
+
+            }
+        }
+    }
+
+    class Handler_1
+    {
+        public void Message(string value)
+        {
+            Console.WriteLine($"Оп хэй а ла лэй!! {value}");
+        }
+    }
+    class Handler_2
+    {
+        public void Message(string value)
+        {
+            Console.WriteLine($"Вася танчит как поц {value}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassCounter<string> counter = new ClassCounter<string>();
+            Handler_1 handler1 = new Handler_1();
+            Handler_2 handler2 = new Handler_2();
+
+            counter.onCount += handler1.Message;
+            counter.onCount += handler2.Message;
+            counter.Count();
+
+        }
+
+    }
+}
